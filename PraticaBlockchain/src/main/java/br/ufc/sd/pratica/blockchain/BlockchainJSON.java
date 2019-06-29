5cf1861662afc59da13b7cc1439d2d581346d6c1
salomao.santos <salomaosilvasantos@gmail.com>
2019-06-29 17:08:38 -0300

    Pratica Blockchain

---
+++ PraticaBlockchain/src/main/java/br/ufc/sd/pratica/blockchain/BlockchainJSON.java
@@ -0,0 +1,107 @@
+package br.ufc.sd.pratica.blockchain;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+
+import br.ufc.sd.pratica.blockchain.model.Vote;
+
+public class BlockchainJSON {
+
+	private static void generateVotes(String filePath) {
+
+		ArrayList<Vote> votes = new ArrayList<Vote>();
+
+		Vote v0 = new Vote(1, "Joao", "0");
+       
+		votes.add(v0);
+		
+		Vote v1 = new Vote(2, "Jose ", v0.getHash());
+		
+		votes.add(v1);
+
+		Vote v2 = new Vote(3, "Maria", v1.getHash());
+
+		votes.add(v2);
+		
+		Vote v3 = new Vote(4, "Joao", v2.getHash());
+
+		votes.add(v3);
+		
+		Vote v4 = new Vote(5, "Jose", v3.getHash());
+
+		votes.add(v4);
+		
+		Vote v5 = new Vote(6, "Jose", v4.getHash());
+
+		votes.add(v5);
+
+		
+		Gson gson = new Gson();
+		
+		try {
+
+			String jsonString = gson.toJson(votes);
+			FileWriter writer = new FileWriter(filePath);
+			writer.write(jsonString);
+			writer.close();
+
+			System.out.println("The votes of election were saved in the folder: " + filePath);
+			
+		} catch (JsonIOException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		
+	}
+
+	private static boolean valid(String filePath) {
+		Gson gson = new Gson();
+
+		try {
+
+			BufferedReader bufferReader = new BufferedReader(new FileReader(filePath));
+
+			Vote[] votes = gson.fromJson(bufferReader, Vote[].class);
+
+			for (int j = votes.length - 1; j > 1; j--) {
+
+				if (!votes[j].getPreviousHash().equalsIgnoreCase(votes[j - 1].getHash())) {
+					
+					return false;
+
+				}
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return true;
+
+	}
+
+	public static void main(String[] args) {
+		
+		String filePath = new File("src/main/resources/votes.json").getAbsolutePath().toString();
+		
+		generateVotes(filePath);
+		
+		boolean isValid = valid(filePath);
+        
+		if(isValid) 
+        	System.out.println("The blockchain is Valid!");
+        else 
+        	System.out.println("The blockchain is Invalid!");
+	}
+
+}
---
+++ PraticaBlockchain/src/main/java/br/ufc/sd/pratica/blockchain/model/Vote.java
@@ -0,0 +1,83 @@
+package br.ufc.sd.pratica.blockchain.model;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+
+import com.google.common.hash.Hashing;
+
+public class Vote {
+
+	private int identifier;
+
+	private String data;
+
+	private String hash;
+
+	private String previousHash;
+
+	
+	private String dateTime;
+
+	public Vote(int identifier, String data, String PreviousHash) {
+
+		super();
+		this.identifier = identifier;
+
+
+		if (identifier == 0) {
+
+			this.previousHash = "0";
+
+		} else {
+
+			this.previousHash = PreviousHash;
+
+		}
+
+		this.hash = getSHA256(identifier, dateTime, data, previousHash);
+
+		this.dateTime = LocalDateTime.now().toString();
+
+		this.data = data;
+ 
+	}
+
+	public Vote(int identifier, String dateTime, String data, String hash, String previousHash) {
+		super();
+		this.identifier = identifier;
+		this.data = data;
+		this.hash = hash;
+		this.previousHash = previousHash;
+		this.dateTime = dateTime; 
+	}
+
+	private String getSHA256(int identifier, String dateTime, String data, String previousHash) {
+
+		CharSequence sequence = Integer.toString(identifier) + dateTime + data + previousHash;
+
+		String sha256 = Hashing.sha256().hashString(sequence, StandardCharsets.UTF_8).toString();
+
+		return sha256;
+	}
+
+	public int getIdentifier() {
+		return identifier;
+	}
+
+	public String getDateTime() {
+		return dateTime;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public String getHash() {
+		return hash;
+	}
+
+	public String getPreviousHash() {
+		return previousHash;
+	}
+
+}
